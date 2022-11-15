320a1326301c24feae3325a6e4c2dcd5c8394655
PaoloCrescenz <crescenzipaolo2@gmail.com>
2022-11-15 17:29:19 +0100

    Milestone 0 e 1

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//Creare l’array di oggetti con le informazioni fornite
+const teamMember = [
+    {
+        name:"Wayne Barnett",
+        rule:"Founder & CEO",
+        img:"wayne-barnett-founder-ceo.jpg",
+    }, {
+        name:"Angela Caroll",
+        rule:"Chief Editor",
+        img:"angela-caroll-chief-editor.jpg",
+    }, {
+        name:"Walter Gordon",
+        rule:"Office Manager",
+        img:"walter-gordon-office-manager.jpg",
+    }, {
+        name:"Angela Lopez",
+        rule:"Social Media Manager",
+        img:"angela-lopez-social-media-manager.jpg",
+    }, {
+        name:"Scott Estrada",
+        rule:"Developer",
+        img:"scott-estrada-developer.jpg",
+    }, {
+        name:"Barbara Ramos",
+        rule:"Graphic Designer",
+        img:"barbara-ramos-graphic-designer.jpg",
+    }
+];
+
+console.log(teamMember);
+
+function addElement () {
+    const element = document.createElement("div");
+    element.appendChild(document.createTextNode('The man who mistook his wife for a hat'));
+    document.getElementbyId('lista').appendChild(element);
+}
